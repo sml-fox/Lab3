0ce6a3d940c4d08291d8145ba2ed82d704ac1922
Solomiia <nadija2379@gmail.com>
2021-12-25 16:23:10 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#define A 6.435
+#define B 1.87
+#define x 1
+
+
+
+main( )
+{
+    float c;
+    double g = 4.751;
+    if ((g < A) & (g > B)) {
+        c = pow(g, 1.4) + B * pow(sin(A / 2), 2);
+        printf("%f", c);
+    }
+    else
+        if ( g < B)
+        {
+            c = x * pow(g, 1.4) +B;
+            printf("%f", c);
+        }
+        
+}
+
+
