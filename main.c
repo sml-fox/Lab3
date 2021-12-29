e23fc8fe7924f0c7a0858372374020620ee98cdb
nadija2379@gmail.com <hmarunka11S>
2021-12-29 11:06:27 +0100

    Add pow

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+
 #define A 6.435
 #define B 1.87
 #define x 1
@@ -10,8 +11,9 @@ main( )
 {
     float c;
     double g = 4.751;
-    if ((g < A) & (g > B)) {
-        c = pow(g, 1.4) + B * pow(sin(A / 2), 2);
+    if ((g < A) & (g > B))
+    {
+        c = pow(g, 1.4) + B * pow(sin(A / 2), 2); // pow - вводить число в ступінь
         printf("%f", c);
     }
     else
